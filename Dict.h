4382c1425b34aeca2240a71e04fe8da6a48ab26e
Jean Carlos Choto <calos_choto090@hotmail.com>
2023-11-23 09:01:57 +0100

    Añadida interfaz genérica Dict

--- Dict.h
+++ Dict.h
@@ -5,7 +5,10 @@
 template <typename V> 
 class Dict {
     public:
-        // ... aquí los métodos virtuales puros
+       virtual void insert(std::string key, V value) = 0;
+	virtual V search(std::string key) = 0;
+	virtual V remove(std::string key) = 0;
+	virtual int entries() = 0;
 };
 
 #endif
