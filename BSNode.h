a66404e3964ca30ab202c7820fb1c90c8178afa4
Jean Carlos Choto <calos_choto090@hotmail.com>
2024-01-10 21:28:32 +0100

    ya esta casi todo

--- BSNode.h
+++ BSNode.h
@@ -6,7 +6,20 @@
 template <typename T> 
 class BSNode {
     public:
-        // miembros públicos
+        T elem;
+	BSNode<T>* left;
+	BSNode<T>* right;
+	BSNode(T elem, BSNode<T>* left = nullptr, BSNode<T>* right = nullptr){
+		this->elem = elem;
+		this->left = nullptr;
+		this->right = nullptr;
+
+
+	}
+	friend std::ostream& operator<<(std::ostream& out, const BSNode<T>& bsn){
+		out << bsn.elem << std::endl;
+		return out;
+	}
     
 };
 
--- Makefile
+++ Makefile
@@ -1,6 +1,11 @@
 bin/testTableEntry: testTableEntry.cpp TableEntry.h
 	mkdir -p bin
 	g++ -o bin/testTableEntry testTableEntry.cpp
-
+bin/testHashTable: testHashTable.cpp Dict.h HashTable.h TableEntry.h
+	mkdir -p bin
+	g++ -o bin/testHashTable testHashTable.cpp Dict.h HashTable.h TableEntry.h
+bin/testBSTree: BSTree.h BSNode.h testBSTree.cpp
+	mkdir -p bin
+	g++ -o bin/testBSTree testBSTree.cpp BSNode.h BSTree.h
 clean:
 	rm -rf *.o *.gch bin
